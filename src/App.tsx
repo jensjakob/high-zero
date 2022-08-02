f50f89013ff233c288fefc79edca1baf70918fdf
Jakob Nanneson <jakob.nanneson@osynlig.se>
2022-08-02 22:06:55 +0200

    Remove logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Lines from "./Lines";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Lines />
-      </header>
+      <Lines />
     </div>
   );
 }
